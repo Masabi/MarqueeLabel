e45cf162560eef3a8bd19ae34c84537569638101
Aleksandra Generowicz <62150130+ale-gen@users.noreply.github.com>
2024-08-15 14:09:51 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,13 +6,14 @@ import PackageDescription
 let package = Package(
     name: "MarqueeLabel",
     platforms: [
-        .iOS(.v11),
-        .tvOS(.v11)
+        .iOS(.v13),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MarqueeLabel",
+            type: .dynamic,
             targets: ["MarqueeLabel"]),
     ],
     targets: [
