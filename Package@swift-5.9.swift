4295fbd8239f27f94006247d98d32ffb064928ef
Aleksandra Generowicz <62150130+ale-gen@users.noreply.github.com>
2024-08-22 13:20:22 +0200

    Update Package@swift-5.9.swift

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -14,7 +14,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MarqueeLabel",
-            type: .dynamic,
             targets: ["MarqueeLabel"]),
     ],
     targets: [
